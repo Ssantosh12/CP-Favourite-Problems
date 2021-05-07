3c24fb6f705aacb971cacd7d548717775ebc09aa
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-05-07 07:38:55 +0530

    Update Jump Game.cpp

--- Leetcode/Jump Game.cpp
+++ Leetcode/Jump Game.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxpos=0,i,j,posreach;
+        //any index is reachable if nums[i]!=0 , now if a 0 comes we have to get past it, we cannot land on it
+        //so while travelling we note the maximum reachable index and this maxpos must be greater than
+        // the index of any 0 element that comes in our way
         for(i=0;i<nums.size()-1;i++){
             if(nums[i]!=0){
                 posreach=i+nums[i];
