5f2fb14519d604fedd16ae216fa995aa2a8e8a93
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-05-18 12:58:26 +0530

    Create The Great Xor.cpp

---
+++ Hackerrank/The Great Xor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  //little bit similar to bit strings problem using knowlwdgw of permutations and combinations
+    int q;
+    scanf("%d",&q);
+    while(q--){
+        long long int x,i,cnt=0,cnd=0,pw;
+        scanf("%lld",&x);
+        for(i=33;i>=0;i--){ //since x<=10^10 , so we start i from 33
+            if((1LL<<i)&x)
+                cnd=1;
+            else{
+                if(cnd==1){
+
+                    cnt=cnt+pow(2,i);
+                }
+            }
+        }
+        printf("%lld\n",cnt);
+    }
+}
