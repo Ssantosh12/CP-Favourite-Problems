4f79560bcf78d7caaf4a0bc4a4508aa37c90b649
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-06-07 19:44:23 +0530

    Create Count Negative numbers in sorted matrix (mlogn).cpp

---
+++ Leetcode/Count Negative numbers in sorted matrix (mlogn).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        //for each row we are gonna use binary search concept 
+        int i,j,left,right,cnt=0,mid;
+        for(i=0;i<grid.size();i++){
+            left=-1;
+            right=grid[i].size()-1;
+            //we have to find index of last positive number
+            while(left!=right){
+                mid=left+right;
+                if(mid&1)
+                    mid++;
+                mid=mid/2;
+                if(grid[i][mid]<0)
+                    right=mid-1;
+                else left=mid;
+            }
+            cnt=cnt+(grid[i].size()-(left+1));
+        }
+        return cnt;
+    }
+};
