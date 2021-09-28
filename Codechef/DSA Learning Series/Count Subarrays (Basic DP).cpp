3f8d88bd84b98cfa306dce149130ca2984c62a7f
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-09-28 21:26:48 +0530

    Create Count Subarrays (Basic DP).cpp

---
+++ Codechef/DSA Learning Series/Count Subarrays (Basic DP).cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d",&t);      //solved using basic concept of non overlapping segment, juts focus on breaking conditions
+    while(t--){
+        long long int n,i,cnt=0,ans=0,prv=-1;
+        scanf("%lld",&n);
+        int a[n];
+        for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+        //non overlapping segment problem
+        for(i=0;i<n;i++){
+            if(a[i]>=prv)
+                cnt++;
+            else{                    //breaking condition
+                ans=ans+(cnt*(cnt+1))/2;
+                cnt=1;
+            }
+
+            if(i==(n-1))
+                    ans=ans+(cnt*(cnt+1))/2;
+
+            prv=a[i];
+        }
+        printf("%lld\n",ans);
+    }
+}
