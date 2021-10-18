7fa70361ca91c42ea2016328f76b6c521e5aae6b
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2021-10-18 22:33:11 +0530

    Update Ternary String.cpp

--- Codeforces/Ternary String.cpp
+++ Codeforces/Ternary String.cpp
@@ -7,16 +7,16 @@ int main()
     int t;
     scanf("%d",&t);
     while(t--){
-        int left=0,right=0,i,mn=200000;
+        int left=0,right=0,i,mn=200000;  //Problem is similar as finding the min lemgth subarray with sum k (Edu section problem)
         bool cnd=false;
-        map<char,int> mp;
+        map<char,int> mp;  //instead of sum that we used in above problem , here we use map to store frequncy, whenever size of map becomes 3 conditions become valid
         string s;
         cin>>s;
         while(right<s.size()){
             mp[s[right]]++;
-            if(mp.size()==3){
+            if(mp.size()==3){   //here subarray became valid 
                 cnd=true;
-                while(mp.size()==3){
+                while(mp.size()==3){   //since we have to find the subarray of minimum length we reduce its length from the left side as much as we can
                     mp[s[left]]--;
                     if(mp[s[left]]==0)
                         mp.erase(s[left]);
@@ -32,5 +32,3 @@ int main()
 
     }
 }
-
-
